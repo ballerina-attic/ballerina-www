4f28400610beee06bade5d7571d90cccd40f6179
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-03-27 06:48:02 -0600

    Go version check

--- website/tools/build.sh
+++ website/tools/build.sh
@@ -1,38 +1,41 @@
 #!/bin/sh
 #nightly build script
 
-echo ".....entering script....."
-mkdir webroot
+echo "**********************GO VERSION********************************************************"
+go version
 
-#two-column-pages
-cp two-column-pages/* ./ -r
-mkdocs build
-rm site/footer.html
-rm site/css site/fonts/ site/img/ site/js/ -r
-mv site/* webroot
+echo "**********************GO VERSION********************************************************"
+# mkdir webroot
 
-#main-pages
+# #two-column-pages
+# cp two-column-pages/* ./ -r
+# mkdocs build
+# rm site/footer.html
+# rm site/css site/fonts/ site/img/ site/js/ -r
+# mv site/* webroot
 
-cp main-pages/* ./ -r
-mkdocs build
-cp site/* webroot -R
+# #main-pages
 
-echo ".....starting BBE...."
+# cp main-pages/* ./ -r
+# mkdocs build
+# cp site/* webroot -R
 
-rm public -r
-mkdir public
-export GOPATH=$PWD
-go get github.com/russross/blackfriday
-go run tools/ballerinaByExample/tools/generate.go
-cp public webroot/ballerina-by-example -r
+# echo ".....starting BBE...."
 
-echo ".....Finishing BBE...."
+# rm public -r
+# mkdir public
+# export GOPATH=$PWD
+# go get github.com/russross/blackfriday
+# go run tools/ballerinaByExample/tools/generate.go
+# cp public webroot/ballerina-by-example -r
 
-#Generate guides
-rm site -r
-cp guides/mkdocs.yml ./
-mkdocs build
-rm site/footer.html
-rm site/css site/fonts/ site/img/ site/js/ -r
-for d in ./site/*/ ; do (pwd && cp "$d"README/index.html "$d"); done
-mv site/ guides
+# echo ".....Finishing BBE...."
+
+# #Generate guides
+# rm site -r
+# cp guides/mkdocs.yml ./
+# mkdocs build
+# rm site/footer.html
+# rm site/css site/fonts/ site/img/ site/js/ -r
+# for d in ./site/*/ ; do (pwd && cp "$d"README/index.html "$d"); done
+# mv site/ guides
