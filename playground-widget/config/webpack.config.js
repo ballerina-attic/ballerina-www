52b81b6ddbacf8c30b37af0246057cb088089d80
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-04-11 22:02:45 +0530

    Remove webpack web-font plugin

--- playground-widget/config/webpack.config.js
+++ playground-widget/config/webpack.config.js
@@ -25,7 +25,6 @@
  const CopyWebpackPlugin = require('copy-webpack-plugin');
  const HtmlWebpackPlugin = require('html-webpack-plugin');
  const CleanWebpackPlugin = require('clean-webpack-plugin');
- const WebfontPlugin = require('webpack-webfont').default;
  
  const isProductionBuild = process.env.NODE_ENV === 'production';
  const hashToUse = isProductionBuild ? 'chunkhash' : 'hash';
