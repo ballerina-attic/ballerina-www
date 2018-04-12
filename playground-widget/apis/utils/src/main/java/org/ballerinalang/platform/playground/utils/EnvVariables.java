6c78705380c9256a34058e2ee8360043aaa2cffd
Kavith Lokuhewage <kaviththiranga@gmail.com>
2018-04-12 18:55:34 +0530

    Fix logic for constructing ack URL

--- playground-widget/apis/utils/src/main/java/org/ballerinalang/platform/playground/utils/EnvVariables.java
+++ playground-widget/apis/utils/src/main/java/org/ballerinalang/platform/playground/utils/EnvVariables.java
@@ -26,5 +26,5 @@ public class EnvVariables {
     public static final String ENV_BPG_REDIS_READ_PORT = "BPG_REDIS_READ_PORT";
     public static final String ENV_BPG_USE_IN_MEMORY_CACHE = "BPG_USE_IN_MEMORY_CACHE";
     public static final String ENV_BPG_LAUNCHER_SELF_URL = "BPG_LAUNCHER_SELF_URL";
-    public static final String ENV_BPG_CONTROLLER_ACK_URL = "BPG_CONTROLLER_ACK_URL";
+    public static final String ENV_BPG_CONTROLLER_INTERNAL_URL = "BPG_CONTROLLER_INTERNAL_URL";
 }
