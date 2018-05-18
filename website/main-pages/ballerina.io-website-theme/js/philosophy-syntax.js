7d065c5a1452f571002bd6057ed40e145c58d231
jeradrutnam <jeradrutnam@outlook.com>
2018-05-18 09:21:00 +0530

    [Fix #840] Philosophy page flickering issue on code switch

--- website/main-pages/ballerina.io-website-theme/js/philosophy-syntax.js
+++ website/main-pages/ballerina.io-website-theme/js/philosophy-syntax.js
@@ -1,11 +1,12 @@
 /*
-We extract the file name from the link text.
-Replace the space with a hyphan and make all text simple case
-For example "REST Services" will become "rest-services"
-Then we look for the file "rest-services.txt" in the samples folder.
-if the data-run attribute is present in the link
-we search for a file "rest-services-run.txt" file as well
-*/
+ * Extract the file name from the link text &
+ * Replace the space with a hyphan and make all text simple case.
+ * For example "REST Services" will become "rest-services".
+ * Then look for the file e.g. "rest-services.txt" in the samples folder.
+ * If the data-run attribute is present in the link
+ * then search for a file e.g. "rest-services-run.txt" file as well.
+ */
+
 var lineHeight = 18;
 var topPadding = 4;
 var codeBoxOffset = 2;
@@ -13,112 +14,154 @@ var codeOutputBoxOffset = 5;
 var editor = null,
     editorRun = null;
 
-var loadData = function(linkText, sectionId, init) {
-    var fileName = linkText.toLowerCase().replace(/\s/g, "-");
-    var codeBlockBgColor = (sectionId == 'integration') ? '#fff' : '#f5f6f6';
+/*
+ * Method to create fileID using the link inner text
+ * @param {string} fileName link inner text
+ */
+var fileId = function(fileName) {
+    return fileName.toLowerCase().replace(/\s/g, "-");
+};
 
-    $('#' + sectionId + ' .text-display').hide();
-    $('#' + sectionId + ' .shell-display').hide();
-    $('#' + sectionId + ' .code-block').hide().removeClass('active');
-
+/*
+ * Method to load sample code by reading .bal, .out files
+ * @param {string} fileName code sample name
+ */
+var loadData = function(fileName) {
     $.ajax({
         url: "../samples/" + fileName + ".out",
         method: "GET",
         success: function(data) {
-            $('#' + fileName + "-shell").text(data).show();
-        },
-        error: function(data) {
-            $('#' + fileName + "-shell").show();
+            $('#' + fileName + "-shell").text(data);
         }
     });
-
-    $('#' + fileName + "-text").show();
-    $('#' + fileName + "-code").show().attr('style', 'display: flex;' +
-        'background: ' + codeBlockBgColor + '; ' +
-        'border-radius: 0; ' +
-        'margin-bottom: 0').addClass('active');
-    $('#' + fileName + "-code > code").show().attr('style', 'white-space:pre; width: 100%;');
 
     $.ajax({
         url: "../samples/" + fileName + ".bal",
         method: "GET",
         success: function(data) {
-            //Set the code to the container
+            // Set the code to the container
             var highlightCode = hljs.highlightAuto;
+
             $('#' + fileName + "-code > code").text(data);
 
-            //Doing the syntax highlighting
+            // Doing the syntax highlighting
             hljs.highlightBlock($('#' + fileName + "-code > code").get(0));
 
-            //Remove any existing line numbers
-            $('#' + sectionId + ' .line-numbers-wrap').remove();
-
-            //If not the fist load ( document ready callback ) we add the line numbers
-            if (!init) {
-                //cont the number of rows
-                //Remove the new line from the end of the text
-                var numberOfLines = data.split(/\r\n|\r|\n/).length;
-                var lines = '<div class="line-numbers-wrap">';
-
-                //Iterate all the lines and create div elements with line number
-                for (var i = 1; i <= numberOfLines; i++) {
-                    lines = lines + '<div class="line-number">' + i + '</div>';
-                }
-                lines = lines + '</div>';
-                //calculate <pre> height and set it to the container
-                var preHeight = numberOfLines * 18 + 20;
-                $('#' + fileName + "-code")
-                    .height(preHeight)
-                    .addClass('ballerina-pre-wrapper')
-                    .prepend($(lines));
-
-                //position code description boxes to align with highlighting area
-                $('#' + fileName + "-code").closest('.code-wrapper').find('.overlay-highlight').remove();
-                $('#' + fileName + '-text ' + '.hTrigger').each(function(i, n) {
-                    var startLine = $(this).attr('data-startLine');
-                    var endLine = $(this).attr('data-endLine');
-                    var overlayStartPosition = topPadding + (startLine - 1) * lineHeight + 30 + codeBoxOffset;
-                    var overlayHeight = (endLine - (startLine - 1)) * lineHeight;
-                    var topPosition = overlayStartPosition;
-                    var hightLighterPosition = topPosition;
-                    var hightlighterId = (fileName + '-highlighter-' + i);
-                    var offset = 0;
-
-                    if ($(this).prev().length >= 0) {
-                        var prevElemBottom = $(this).prev().height() + parseInt($(this).prev().css('top')) + 20;
-
-                        if ($(this).hasClass('cOutputDesription')) {
-                            topPosition = (($('#' + fileName + '-code').height() + overlayStartPosition) + codeOutputBoxOffset);
-                        }
-
-                        hightLighterPosition = topPosition;
-
-                        if (topPosition < prevElemBottom) {
-                            topPosition = prevElemBottom;
-                        }
-                    }
-
-                    $(this)
-                        .css('top', topPosition)
-                        .attr('data-toggle-highlight', hightlighterId);
-
-                    $(this)
-                        .closest('.container')
-                        .find('.code-wrapper')
-                        .prepend('<div id="' + hightlighterId + '" class="overlay-highlight"></div>');
-
-                    $('#' + hightlighterId)
-                        .css('top', ((hightLighterPosition - 30) + offset))
-                        .css('height', overlayHeight);
-                });
-
-                var $lastCodeDescriptionBox = $('#' + fileName + '-text ' + '.hTrigger:last-child');
-                var codeboxContainerHeight = $lastCodeDescriptionBox.height() + parseInt($lastCodeDescriptionBox.css('top')) + 20;
-                $('#' + fileName + '-text').css('height', codeboxContainerHeight);
-            }
+            addLineNumbers(fileName, data);
+            addCodeHighlights(fileName);
         }
     });
+};
 
+/*
+ * Method to toggle code samples
+ * @param {string} name code sample name
+ * @param {string} sectionId code group/sample section ID
+ */
+var toggleCodeBlock = function(name, sectionId) {
+    var fileName = fileId(name);
+
+    $('#' + sectionId + ' .code-block-text').hide();
+    $('#' + sectionId + ' .code-block').css('display', 'none').removeClass('active');
+    $('#' + sectionId + ' .shell-display').hide();
+
+    $('#' + fileName + "-text").show();
+    $('#' + fileName + "-code").css('display', 'flex').addClass('active');
+    $('#' + fileName + "-shell").show();
+
+    updateCodeDescriptionPosition(fileName);
+};
+
+/*
+ * Method to position code description boxes to align with highlighting area
+ * @param {string} fileName code sample name
+ */
+var updateCodeDescriptionPosition = function(fileName) {
+    $('#' + fileName + '-text ' + '.hTrigger').each(function(i, n) {
+        var startLine = $(this).attr('data-startLine');
+        var endLine = $(this).attr('data-endLine');
+        var overlayStartPosition = topPadding + (startLine - 1) * lineHeight + 30 + codeBoxOffset;
+        var overlayHeight = (endLine - (startLine - 1)) * lineHeight;
+        var topPosition = overlayStartPosition;
+        var hightLighterPosition = topPosition;
+        var hightlighterId = (fileName + '-highlighter-' + i);
+        var offset = 0;
+
+        if ($(this).prev().length > 0) {
+            var prevElemBottom = $(this).prev().height() + parseInt($(this).prev().css('top')) + 20;
+
+            hightLighterPosition = topPosition;
+
+            if ($(this).hasClass('cOutputDesription')) {
+                topPosition = (($('#' + fileName + '-code').height() + overlayStartPosition) + codeOutputBoxOffset);
+                hightLighterPosition = (overlayStartPosition + codeOutputBoxOffset);
+            }
+
+            if (topPosition < prevElemBottom) {
+                topPosition = prevElemBottom;
+            }
+        }
+
+        $(this).css('top', topPosition);
+
+        $('#' + hightlighterId).css({
+            'top': ((hightLighterPosition - 30) + offset),
+            'left': 0,
+            'height': overlayHeight
+        });
+    });
+
+    var $lastCodeDescriptionBox = $('#' + fileName + '-text ' + '.hTrigger:last-child');
+    var codeboxContainerHeight = $lastCodeDescriptionBox.height() + parseInt($lastCodeDescriptionBox.css('top')) + 20;
+    $('#' + fileName + '-text').css('height', codeboxContainerHeight);
+};
+
+/*
+ * Method to add line numbers to code block
+ * @param {string} fileName code sample name
+ * @param {string} data code sample
+ */
+var addLineNumbers = function(fileName, data) {
+    // Remove any existing line numbers
+    $('#' + fileName + '-code .line-numbers-wrap').remove();
+
+    // Count the number of rows
+    // Remove the new line from the end of the text
+    var numberOfLines = data.split(/\r\n|\r|\n/).length;
+    var lines = '<div class="line-numbers-wrap">';
+
+    // Iterate all the lines and create div elements with line number
+    for (var i = 1; i <= numberOfLines; i++) {
+        lines = lines + '<div class="line-number">' + i + '</div>';
+    }
+    lines = lines + '</div>';
+
+    var preHeight = numberOfLines * 18 + 20;
+    $('#' + fileName + "-code")
+        .addClass('ballerina-pre-wrapper')
+        .prepend($(lines));
+};
+
+/*
+ * Method to add code segement highlighters
+ * @param {string} fileName code sample name
+ */
+var addCodeHighlights = function(fileName) {
+    $('#' + fileName + '-text ' + '.hTrigger').each(function(i, n) {
+        var hightlighterId = (fileName + '-highlighter-' + i);
+
+        if ($(this).hasClass('cOutputDesription')) {
+            $('#' + fileName + '-shell')
+                .prepend('<div id="' + hightlighterId + '" class="overlay-highlight"></div>');
+        } else {
+            $('#' + fileName + '-code')
+                .prepend('<div id="' + hightlighterId + '" class="overlay-highlight"></div>');
+        }
+
+        $(this).attr('data-toggle-highlight', hightlighterId);
+    });
+
+    updateCodeDescriptionPosition(fileName);
 };
 
 function setTooltip(btn, message) {
@@ -133,29 +176,47 @@ function hideTooltip(btn) {
 }
 
 $(document).ready(function() {
-    //Load data on click callback
-    $('#nativeLanguage li.links').click(function() {
-        loadData($(this).text(), 'nativeLanguage', false);
-        $('#nativeLanguage li.links').removeClass('cActive');
-        $(this).addClass('cActive');
+
+    /*
+     * Load sample codes on page load
+     */
+    $('li.links').each(function(i, elem) {
+        var fileName = fileId($(elem).text());
+        var sectionId = $(elem).closest('.codeSampleBoxes').attr('id');
+
+        loadData(fileName, sectionId);
     });
-    $('#integration li.links').click(function() {
-        loadData($(this).text(), 'integration', false);
-        $('#integration li.links').removeClass('cActive');
+
+    /*
+     * Toggle code samples on links click
+     */
+    $('li.links').click(function() {
+        var fileName = fileId($(this).text());
+        var sectionId = $(this).closest('.codeSampleBoxes').attr('id');
+
+        toggleCodeBlock(fileName, sectionId);
+
+        $(this).closest('.codeSampleBoxes').find('li.links').removeClass('cActive');
         $(this).addClass('cActive');
     });
 
-    //Load data on page load
-    loadData($('#nativeLanguage li.first').text(), 'nativeLanguage', false);
-    loadData($('#integration li.first').text(), 'integration', false);
+    /*
+     * Toggle first sample code on page load
+     */
+    toggleCodeBlock($('#nativeLanguage li.first').text(), 'nativeLanguage');
+    toggleCodeBlock($('#integration li.first').text(), 'integration');
+
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-        var target = $(e.target).attr("href") // activated tab
+        var target = $(e.target).attr("href"); // activated tab
         if (target === "#3b") {
             $('#type-system-code .line-numbers-wrap').remove();
-            loadData('type-system', 'dummyXoXo', false);
+            loadData('type-system');
         }
     });
 
+    /*
+     * On code descriptions hover toggle relative code segment highlighter
+     */
     $('.codeSampleBoxes .hTrigger').hover(function(e) {
             $('#' + $(this).data('toggle-highlight')).css('opacity', 0.2);
         },
@@ -163,7 +224,6 @@ $(document).ready(function() {
             $('.overlay-highlight').css('opacity', 0);
         }
     );
-
 
     /*
      * Code hints hightlighting method
@@ -189,7 +249,6 @@ $(document).ready(function() {
     $codeWrapper.on('mouseup mouseleave', function(e) {
         $('.overlay-highlight', e.currentTarget).css('z-index', 3);
     });
-
 
     /*
      * Register "copy to clipboard" event to elements with "copy" class
