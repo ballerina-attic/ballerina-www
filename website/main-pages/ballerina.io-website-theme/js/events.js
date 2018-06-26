515c21eb1585c2cf6dff15324469e4f265afa348
madhuramendis <madhuramendis@gmail.com>
2018-06-26 12:10:55 +0530

    Update events page and webinar pages

---
+++ website/main-pages/ballerina.io-website-theme/js/events.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$( ".cWebinarList tr:last" ).addClass("cLastTableRow");
+        $( ".cMeetupsList tr:last" ).addClass("cLastTableRow");
+        $( ".cConferencesList tr:last" ).addClass("cLastTableRow");
+        $( ".cGLList tr:last" ).addClass("cLastTableRow");
+        
+});
