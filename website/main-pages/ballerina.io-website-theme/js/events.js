e96fca5ea1d463246dac3312470c5ee4892c50e2
madhuramendis <madhuramendis@gmail.com>
2018-07-06 12:38:56 +0530

    Update events page

--- website/main-pages/ballerina.io-website-theme/js/events.js
+++ website/main-pages/ballerina.io-website-theme/js/events.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
         $( ".cConferencesList tr:last" ).addClass("cLastTableRow");
         $( ".cGLList tr:last" ).addClass("cLastTableRow");
         
+        $( ".cWebinarList tr:first" ).addClass("cFirstTableRow");
+        $( ".cMeetupsList tr:first" ).addClass("cFirstTableRow");
+        $( ".cConferencesList tr:first" ).addClass("cFirstTableRow");
+        $( ".cGLList tr:first" ).addClass("cFirstTableRow");
+        
 });
