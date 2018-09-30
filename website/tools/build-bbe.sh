d056fcae3f12771b220075c17084ae5afcbc834a
thimuth <thimuth@gmail.com>
2018-09-30 17:17:52 +0530

    resources added for the 0.982.0 release

--- website/tools/build-bbe.sh
+++ website/tools/build-bbe.sh
@@ -5,6 +5,6 @@ export GOPATH=$3
 go get github.com/russross/blackfriday
 rm target/dependencies/ballerina-examples -rf
 #git clone https://github.com/ballerina-platform/ballerina-examples target/dependencies/ballerina-examples
-git clone --branch v0.981.0 https://github.com/ballerina-platform/ballerina-examples target/dependencies/ballerina-examples
+git clone --branch v0.982.0 https://github.com/ballerina-platform/ballerina-examples target/dependencies/ballerina-examples
 go run tools/ballerinaByExample/tools/generate.go "target/dependencies/ballerina-examples" $2
 echo "....Completed building BBE Site...."
