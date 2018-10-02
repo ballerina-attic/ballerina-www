96fc974fe2ded21018eb94c4684522037359f2ef
thimuth <thimuth@gmail.com>
2018-10-02 15:24:32 +0530

    Add missing BBE to the list temporarily

--- website/tools/build-bbe.sh
+++ website/tools/build-bbe.sh
@@ -6,5 +6,8 @@ go get github.com/russross/blackfriday
 rm target/dependencies/ballerina-examples -rf
 #git clone https://github.com/ballerina-platform/ballerina-examples target/dependencies/ballerina-examples
 git clone --branch v0.982.0 https://github.com/ballerina-platform/ballerina-examples target/dependencies/ballerina-examples
+mv target/dependencies/ballerina-examples/examples/abstract-objects/abstract-objects.bal target/dependencies/ballerina-examples/examples/abstract-objects/abstract_objects.bal
+mv target/dependencies/ballerina-examples/examples/abstract-objects/abstract-objects.description target/dependencies/ballerina-examples/examples/abstract-objects/abstract_objects.description
+mv target/dependencies/ballerina-examples/examples/abstract-objects/abstract-objects.out target/dependencies/ballerina-examples/examples/abstract-objects/abstract_objects.out
 go run tools/ballerinaByExample/tools/generate.go "target/dependencies/ballerina-examples" $2
 echo "....Completed building BBE Site...."
