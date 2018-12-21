777959b11319250f351c7979c7945772f0ae8d9f
thimuth <thimuth@gmail.com>
2018-12-21 14:15:39 +0530

    0.990.1 release resources

--- website/tools/build-bbe.sh
+++ website/tools/build-bbe.sh
@@ -10,7 +10,7 @@ rm target/dependencies/ballerina-examples -rf
 #get BBE from the language master branch
 rm ballerina-lang -rf
 git clone https://github.com/ballerina-platform/ballerina-lang
-git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v0.990.0
+git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v0.990.1
 mkdir -p target/dependencies/ballerina-examples/
 mv ballerina-lang/examples target/dependencies/ballerina-examples/examples/
 rm ballerina-lang -r
@@ -18,21 +18,21 @@ rm ballerina-lang -r
 #get BBE from BallerinaX
 rm docker -rf
 git clone https://github.com/ballerinax/docker
-git --git-dir=docker/.git --work-tree=docker/ checkout v0.990.0
+git --git-dir=docker/.git --work-tree=docker/ checkout v0.990.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv docker/docker-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm docker -r
 
 rm kubernetes -rf
 git clone https://github.com/ballerinax/kubernetes
-git --git-dir=kubernetes/.git --work-tree=kubernetes/ checkout v0.990.0
+git --git-dir=kubernetes/.git --work-tree=kubernetes/ checkout v0.990.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv kubernetes/kubernetes-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm kubernetes -r
 
 rm jdbc -rf
 git clone https://github.com/ballerinax/jdbc
-git --git-dir=jdbc/.git --work-tree=jdbc/ checkout v0.990.0
+git --git-dir=jdbc/.git --work-tree=jdbc/ checkout v0.990.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv jdbc/jdbc-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm jdbc -r
