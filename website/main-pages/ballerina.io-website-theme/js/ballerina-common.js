098fffbbd6751c0eeb49beda474cb8bc8c4955ef
thimuth <thimuth@gmail.com>
2019-09-09 14:46:44 +0530

    added new versions to the dropdown

--- website/main-pages/ballerina.io-website-theme/js/ballerina-common.js
+++ website/main-pages/ballerina.io-website-theme/js/ballerina-common.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
         '<li class="toctree-l1"><a class="cBioTopLink" href="https://central.ballerina.io/" target="_blank">Central</a></li>' +
         '<li class="toctree-l1" id="openli"><a class="cBioTopLink" href="/community">Community</a></li>' +
         '<li class="toctree-l1" id="helpli"><a class="cBioTopLink" href="https://blog.ballerina.io">Blog</a></li>' +
-        '<li class="cVersionItem"><div class="cVersionContainer"><lable class="cVlable">Version</lable><select name="versions" id="versions" class="select-css"><option value="http://ballerina.io'+pathValue+'">Stable</option><option value="https://v1-0-0-alpha.ballerina.io'+pathValue+'" selected="selected">1.0.0-alpha</option> </select></div></li>' +
+        '<li class="cVersionItem"><div class="cVersionContainer"><lable class="cVlable">Version</lable><select name="versions" id="versions" class="select-css"><option value="http://v1-0.ballerina.io'+pathValue+'">1.0</option><option value="https://v0-991.ballerina.io'+pathValue+'" selected="selected">0.991</option> </select></div></li>' +
         '<li class="toctree-l1"><a class="cSerachIcon" href="#"><img src="/img/search.svg"/></a>' +
         '<div class="cSearchBoxTopMenu">' +
         '<div role="search">' +
@@ -241,8 +241,8 @@ $(document).ready(function() {
     var pathValue = window.location.pathname;
     var version =    '<div class="cVersionContainer"><lable class="cVlable">Version</lable>' +
     '<select name="versions" id="versions" class="select-css">' +
-    '<option value="http://ballerina.io'+pathValue+'">Stable</option>' +
-    '<option value="https://v1-0-0-alpha.ballerina.io'+pathValue+'" selected="selected">1.0.0-alpha</option>' +
+    '<option value="http://v1-0.ballerina.io'+pathValue+'">1.0</option>' +
+    '<option value="https://v0-991.ballerina.io'+pathValue+'" selected="selected">0.991</option>' +
     '</select>'+
     '</div>';
 
