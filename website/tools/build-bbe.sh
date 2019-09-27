089a1c76108cd607b52d59721ad318baf1728071
thimuth <thimuth@gmail.com>
2019-09-27 10:42:35 +0530

    1.0.1 resources added

--- website/tools/build-bbe.sh
+++ website/tools/build-bbe.sh
@@ -10,7 +10,7 @@ rm target/dependencies/ballerina-examples -rf
 #get BBE from the language master branch
 rm ballerina-lang -rf
 git clone https://github.com/ballerina-platform/ballerina-lang
-git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v1.0.0
+git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v1.0.1
 mkdir -p target/dependencies/ballerina-examples/
 mv ballerina-lang/examples target/dependencies/ballerina-examples/examples/
 rm ballerina-lang -r
@@ -18,28 +18,28 @@ rm ballerina-lang -r
 #get BBE from BallerinaX
 rm docker -rf
 git clone https://github.com/ballerinax/docker
-git --git-dir=docker/.git --work-tree=docker/ checkout v1.0.0
+git --git-dir=docker/.git --work-tree=docker/ checkout v1.0.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv docker/docker-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm docker -r
 
 rm kubernetes -rf
 git clone https://github.com/ballerinax/kubernetes
-git --git-dir=kubernetes/.git --work-tree=kubernetes/ checkout v1.0.0
+git --git-dir=kubernetes/.git --work-tree=kubernetes/ checkout v1.0.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv kubernetes/kubernetes-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm kubernetes -r
 
 rm jdbc -rf
 git clone https://github.com/ballerinax/jdbc
-git --git-dir=jdbc/.git --work-tree=jdbc/ checkout v1.0.0
+git --git-dir=jdbc/.git --work-tree=jdbc/ checkout v1.0.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv jdbc/jdbc-extension-examples/examples/* target/dependencies/ballerina-examples/examples/
 rm jdbc -r
 
 rm awslambda -rf
 git clone https://github.com/ballerinax/awslambda
-git --git-dir=awslambda/.git --work-tree=awslambda/ checkout v1.0.0
+git --git-dir=awslambda/.git --work-tree=awslambda/ checkout v1.0.1
 mkdir -p target/dependencies/ballerina-examples/examples
 mv awslambda/awslambda-examples/examples/* target/dependencies/ballerina-examples/examples/
 mkdir -p target/dependencies/ballerina-examples/examples/awslambda-deployment
